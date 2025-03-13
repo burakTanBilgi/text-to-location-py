f415feb0b2ae892c2a3118f74cf05ee4919e473c
burakTanBilgi <buraktan.bilgi@yahoo.com>
2025-03-13 07:12:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
+from geopy.extra.rate_limiter import RateLimiter
+
+def get_location_coordinates(place_name):
+    try:
+        geolocator = Nominatim(user_agent="your_app_name", timeout=10)
+        geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+        location = geocode(place_name)
+        if location:
+            return location.latitude, location.longitude
+        else:
+            print("Yer bulunamadı.")
+            return None
+    except GeocoderTimedOut:
+        print("Zaman aşımı hatası.")
+        return None
+    except GeocoderServiceError:
+        print("Servis hatası.")
+        return None
+
+if __name__ == "__main__":
+    place_name = input("Yer ismi girin: ")
+    coordinates = get_location_coordinates(place_name)
+    if coordinates:
+        print(f"{place_name} koordinatları: Enlem = {coordinates[0]}, Boylam = {coordinates[1]}")
